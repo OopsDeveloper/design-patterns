d7aca287155cfe2b255428fe5cfc802b48adb933
Go YongSu <itgys91@kbet.or.kr>
2023-04-19 18:30:36 +0900

    퍼사드 패턴

--- src/main/java/me/whiteship/designpatterns/_02_structural_patterns/_10_facade/_01_before/Client.java
+++ src/main/java/me/whiteship/designpatterns/_02_structural_patterns/_10_facade/_01_before/Client.java
@@ -11,6 +11,7 @@ import java.util.Properties;
 public class Client {
     // 현재 클라이언트 코드들이 서로간에 얽혀 있다.
     // 현재는 세션이 있어야 메세지를 만들 수 있고 메세지가 있어야 트랜스포트를 할 수 있다.
+    // 지금 코드들은 API 라이브러리에 대한 깊이 있는 학습이 필요.
     public static void main(String[] args) {
         String to = "keesun@whiteship.me";
         String from = "whiteship@whiteship.me";
--- src/main/java/me/whiteship/designpatterns/_02_structural_patterns/_10_facade/_02_after/Client.java
+++ src/main/java/me/whiteship/designpatterns/_02_structural_patterns/_10_facade/_02_after/Client.java
@@ -7,6 +7,8 @@ public class Client {
         // 이렇게 하는게 나쁘지 않은 선택이긴하다.
         // 클라이언트가 이메일을 여러군대서 보낸다고 한다면 의미가 있다.
         // 한 곳으로 모아놨다고 하는 것만으로도 의미가 있다.
+        // 코드가 읽기 편한 리팩토링이라 생각한다.
+        // API 라이브러리에 대한 깊이 있는 학습이 필요 없다.
         EmailSettings emailSettings = new EmailSettings();
         emailSettings.setHost("127.0.0.1");
 
