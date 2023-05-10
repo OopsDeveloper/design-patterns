8f05f0e3a71cd4822abac489840438a64c90ad2a
고용수 <ajemxkem@gmail.com>
2023-05-10 19:27:48 +0900

    테스트

--- src/main/java/me/whiteship/designpatterns/_03_behavioral_patterns/_18_memento/_01_before/Client.java
+++ src/main/java/me/whiteship/designpatterns/_03_behavioral_patterns/_18_memento/_01_before/Client.java
@@ -6,6 +6,7 @@ public class Client {
         Game game = new Game();
         game.setRedTeamScore(10);
         game.setBlueTeamScore(20);
+        /*test*/
 
         int blueTeamScore = game.getBlueTeamScore();
         int redTeamScore = game.getRedTeamScore();
